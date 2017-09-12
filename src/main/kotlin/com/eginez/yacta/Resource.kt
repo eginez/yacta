1459b29d39feee120a879c49e571929456fbbab9
Esteban G <esteban.ginez@oracle.com>
2017-09-12 13:47:44 -0700

    Non neste object storage dsl working

--- src/main/kotlin/com/eginez/yacta/Resource.kt
+++ src/main/kotlin/com/eginez/yacta/Resource.kt
@@ -5,51 +5,103 @@ import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider
 import com.oracle.bmc.core.ComputeClient
 import com.oracle.bmc.core.model.LaunchInstanceDetails
 import com.oracle.bmc.core.requests.LaunchInstanceRequest
+import com.oracle.bmc.objectstorage.ObjectStorageClient
+import com.oracle.bmc.objectstorage.model.CreateBucketDetails
+import com.oracle.bmc.objectstorage.requests.CreateBucketRequest
+import com.oracle.bmc.objectstorage.requests.GetNamespaceRequest
+import com.oracle.bmc.objectstorage.requests.PutObjectRequest
+import java.io.File
 
 @DslMarker
 annotation class ResourceMarker
 
+
+val executionGraph : MutableList<Any> = mutableListOf()
 @ResourceMarker
 class Resource {
 
-    var computeClient: ComputeClient? = null
-    val builder = LaunchInstanceDetails.Builder()
-    var availabiltyDomains: String = ""
-    var compartmentId: String = ""
+    var profile = "DEFAULT"
+    var filePath = "~/.oraclebmc/config"
+    var provider = ConfigFileAuthenticationDetailsProvider(filePath, profile)
+    val DEFAULT_REGION = Region.US_PHOENIX_1
+    var region: Region? = null
+
+
 
     fun compute(fn: Resource.() -> Unit): Unit {
-        this.computeClient = createComputeClient()
-        createCompute()
+        println("Creating compute resource");
     }
 
-    fun vnicDetails(fn: Resource.() -> Unit): Unit {
-
-
-    }
-
-
-    private fun createComputeClient(): ComputeClient {
-        var profile = "DEFAULT"
-        var filePath = "~/.oraclebmc/config"
-        var provider = ConfigFileAuthenticationDetailsProvider(filePath, profile)
-        var client = ComputeClient(provider)
-        client.setRegion(Region.US_ASHBURN_1)
-        return client
+    fun casper(fn: Resource.() -> Unit) {
+        fn()
 
     }
 
-    private fun createCompute() {
-        val builder = LaunchInstanceRequest.builder()
+    fun bucket(fn: BucketResource.() -> Unit) {
+        val client = ObjectStorageClient(provider)
+        client.setRegion(region)
+        val n = BucketResource(client)
+        n.apply(fn)
+        n.create()
     }
 }
 
-fun main(args: Array<String>) {
-    val c = Resource()
-    c.compute {
-        availabiltyDomains = "asdf"
-        compartmentId = "asdf"
-        vnicDetails {
 
+data class BucketResource(val client: ObjectStorageClient) {
+    var name: String = ""
+    var compartmentId: String = ""
+    var accessType : CreateBucketDetails.PublicAccessType? = null
+    var namespace: String? = null
+
+    override fun toString(): String {
+        return "BucketResource(namespace='${namespace}' name='$name', compartmentId='$compartmentId', accessType=$accessType)"
+    }
+
+    fun defaultNamespace(): String {
+        return client.getNamespace(GetNamespaceRequest.builder().build()).value
+    }
+
+    fun create() {
+        if(namespace.isNullOrBlank()) {
+            namespace = defaultNamespace()
         }
+
+        println("Creating bucket ${this}")
+        val details = CreateBucketDetails(name, compartmentId, emptyMap(), accessType)
+        val request = CreateBucketRequest.builder()
+                .namespaceName(namespace)
+                .createBucketDetails(details)
+                .build()
+        client.createBucket(request)
+    }
+
+    fun obj(fn: ObjectResource.() -> Unit) {
+        val o = ObjectResource(client, this)
+        o.apply(fn)
+        o.create()
+
+    }
+
+}
+
+data class ObjectResource(val client: ObjectStorageClient, val parentBucket: BucketResource) {
+    var name: String = ""
+    var accessType : CreateBucketDetails.PublicAccessType? = null
+    var namespace: String? = null
+    var file = File("/Users/eginez/Documents/images/nightsky_1.jpeg")
+
+    fun create() {
+        val request = PutObjectRequest.builder()
+                .bucketName(parentBucket.name)
+                .namespaceName(parentBucket.namespace)
+                .objectName(name)
+                .contentLength(file.length())
+                .putObjectBody(file.inputStream())
+                .build()
+
+        client.putObject(request)
     }
 }
+
+
+
--- src/test/kotlin/com/eginez/yacta/ResourceTest.kt
+++ src/test/kotlin/com/eginez/yacta/ResourceTest.kt
@@ -1,13 +1,76 @@
 package com.eginez.yacta
 
+import  com.oracle.bmc.objectstorage.model.CreateBucketDetails.PublicAccessType.*
 import org.junit.Test
 
 class ResourceTest {
+
     @Test
-    fun testBasic() {
+    fun testCreateBucket() {
         val c = Resource()
-        c.compute {
-            assert(true)
+
+        ////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////
+
+        c.casper {
+            region = DEFAULT_REGION
+            bucket {
+                name = "DSLBucketOne"
+                compartmentId = "ocid1.compartment.oc1..aaaaaaaa6amlftmhyeeeil54oybmd5rizcruqirk73lcm3n45a5sib5ucbxa"
+                accessType = ObjectRead
+                /*
+                obj {
+                    name = "foto1.jpg"
+                }
+                */
+            }
         }
+
+        ////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////
+    }
+
+    @Test
+    fun testCreateMultiple() {
+        val c = Resource()
+
+        ////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////
+
+        c.casper {
+            region = DEFAULT_REGION
+            (1..5).forEach { num ->
+                bucket {
+                    name = "DSLBucket$num"
+                    compartmentId = "ocid1.compartment.oc1..aaaaaaaa6amlftmhyeeeil54oybmd5rizcruqirk73lcm3n45a5sib5ucbxa"
+                    accessType = ObjectRead
+                }
+            }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////
+    }
+
+    @Test
+    fun testBucketAndObject() {
+        val c = Resource()
+
+        ////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////
+
+        c.casper {
+            region = DEFAULT_REGION
+            bucket {
+                name = "DSLBucket"
+                compartmentId = "ocid1.compartment.oc1..aaaaaaaa6amlftmhyeeeil54oybmd5rizcruqirk73lcm3n45a5sib5ucbxa"
+                accessType = ObjectRead
+                obj {
+                    name = "foto1.jpg"
+                }
+            }
+        }
+        ////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////
     }
 }
